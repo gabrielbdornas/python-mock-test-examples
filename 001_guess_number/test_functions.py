d14b81c87a96afe7158dd09574fcd2d66a9e185f
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-03-02 08:34:44 -0300

    Add add function and adjust modules names

--- 001_guess_number/test_functions.py
+++ 001_guess_number/test_functions.py
@@ -1,18 +1,20 @@
 # Not need third party libraries instalation
 from unittest import mock
 import pytest
-from main import guess_number
+from guess_number import guess_number
 
 @pytest.mark.parametrize(
-                          "input,expect",
-                          [(1, "You lost!")],
-                          [(2, "You lost!")],
-                          [(3, "You lost!")],
-                          [(4, "You lost!")],
-                          [(5, "You lost!")],
-                          [(6, "You lost!")],
-                          )
-@mock.patch("main.roll_dice") # "dice.roll_dice" will call the real function and the test will fail
+                          "_input,expect",
+                          [
+                           (1, "You lost!"),
+                           (2, "You lost!"),
+                           (2, "You lost!"),
+                           (3, "You won!"),
+                           (4, "You lost!"),
+                           (5, "You lost!"),
+                           (6, "You lost!"),
+                          ])
+@mock.patch("guess_number.roll_dice") # "dice.roll_dice" will call the real function and the test will fail
 def test_gues_number(mock_roll_dice, _input, expect):
   mock_roll_dice.return_value=3
   assert guess_number(_input) == expect
